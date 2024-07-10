96f118f8d19131bb05e69e60fecd3b4ed177cc8e
pushprajjaiswal <pushprajapp12@gmail.com>
2024-07-10 22:12:13 +0530

    Basic TS + Express setup Done

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import serverConfig from './config/serverConfig';
+
+const app  = express();
+
+app.listen(serverConfig.PORT, () => {
+    console.log(`Server is started at *:${serverConfig.PORT}`);
+    console.log("Wow bhaiya");
+    
+})
