012ff2d7fb1e84b247e2c2feb50c6ef2827ca44c
pushprajjaiswal <pushprajapp12@gmail.com>
2025-01-03 11:01:54 +0530

    Basic api setup done

---
+++ src/controller/pingController.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from 'express';
+
+export const pingCheck = (req: Request, res: Response) => {
+    // console.log(req.url); 
+    res.status(200).json({ message: 'Ping! check ok' });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 
 import serverConfig from './config/serverConfig';
+import apiRouter from './routes';
 
 const app = express();
 
+app.use('/api', apiRouter);
+
 app.listen(serverConfig.PORT, () => {
   console.log(`Server is started at *:${serverConfig.PORT}`);
-  console.log("Wow bhaiya");
 });
